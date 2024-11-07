f29626413bb3d4f76f72c8df2309619647d71ec9
WarlOrd <tycoon111222333@gmail.com>
2024-11-07 15:58:07 +0300

    added token refresh (not tested)

--- internal/adapters/controller/api/v1/user.go
+++ internal/adapters/controller/api/v1/user.go
@@ -3,12 +3,15 @@ package v1
 import (
 	"context"
 	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/viper"
+	"time"
 	"webTemplate/cmd/app"
 	"webTemplate/internal/adapters/controller/api/validator"
 	"webTemplate/internal/adapters/database/postgres"
 	"webTemplate/internal/domain/dto"
 	"webTemplate/internal/domain/entity"
 	"webTemplate/internal/domain/service"
+	"webTemplate/internal/domain/utils/auth"
 )
 
 type UserService interface {
@@ -17,6 +20,7 @@ type UserService interface {
 
 type TokenService interface {
 	GenerateAuthTokens(c context.Context, userID string) (*dto.AuthTokens, error)
+	GenerateToken(ctx context.Context, userID string, expires time.Time, tokenType string) (*entity.Token, error)
 }
 
 type UserHandler struct {
@@ -94,7 +98,68 @@ func (h UserHandler) register(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusCreated).JSON(response)
 }
 
+// register godoc
+// @Summary      Refresh the access token
+// @Description  Get a new access token using a valid refresh token
+// @Tags         user
+// @Accept       json
+// @Produce      json
+// @Param        body body  dto.Token true  "Access token object"
+// @Success      200  {object}  dto.Token
+// @Failure      400  {object}  dto.HTTPError
+// @Failure      403  {object}  dto.HTTPError
+// @Failure      500  {object}  dto.HTTPError
+// @Router       /user/refresh [post]
+func (h UserHandler) refreshToken(c *fiber.Ctx) error {
+	var accessTokenDTO dto.Token
+
+	if err := c.BodyParser(&accessTokenDTO); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(dto.HTTPError{
+			Code:    fiber.StatusBadRequest,
+			Message: err.Error(),
+		})
+	}
+
+	if errValidate := h.validator.ValidateData(accessTokenDTO); errValidate != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(dto.HTTPError{
+			Code:    fiber.StatusBadRequest,
+			Message: errValidate.Error(),
+		})
+	}
+
+	userID, errToken := auth.VerifyToken(accessTokenDTO.Token, viper.GetString("service.backend.jwt.secret"), auth.TokenTypeAccess)
+
+	if errToken != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(dto.HTTPError{
+			Code:    fiber.StatusUnauthorized,
+			Message: errToken.Error(),
+		})
+	}
+
+	expTime := time.Now().UTC().Add(time.Minute * time.Duration(viper.GetInt("service.backend.jwt.access-token-expiration")))
+
+	newAccess, errNewAccess := h.tokenService.GenerateToken(c.Context(),
+		userID,
+		expTime,
+		auth.TokenTypeAccess)
+
+	if errNewAccess != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(dto.HTTPError{
+			Code:    fiber.StatusInternalServerError,
+			Message: errNewAccess.Error(),
+		})
+	}
+
+	response := dto.Token{
+		Token:   newAccess.Token,
+		Expires: expTime,
+	}
+
+	return c.Status(fiber.StatusOK).JSON(response)
+}
+
 func (h UserHandler) Setup(router fiber.Router) {
 	userGroup := router.Group("/user")
 	userGroup.Post("/register", h.register)
+	userGroup.Post("/refresh", h.refreshToken)
 }
