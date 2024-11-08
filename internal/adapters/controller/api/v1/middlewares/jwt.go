b35a12097b8536e5bf008b70c62ea4e5a5cd0718
qushedo <123452935+qushedo@users.noreply.github.com>
2024-11-08 12:54:41 +0300

    add: verify token middleware

--- internal/adapters/controller/api/v1/middlewares/jwt.go
+++ internal/adapters/controller/api/v1/middlewares/jwt.go
@@ -3,11 +3,13 @@ package middlewares
 import (
 	"context"
 	"github.com/gofiber/fiber/v2"
-	"strings"
+	"github.com/spf13/viper"
 	"webTemplate/cmd/app"
 	"webTemplate/internal/adapters/database/postgres"
+	"webTemplate/internal/domain/common/errorz"
 	"webTemplate/internal/domain/entity"
 	"webTemplate/internal/domain/service"
+	"webTemplate/internal/domain/utils/auth"
 )
 
 type UserService interface {
@@ -28,43 +30,44 @@ func NewMiddlewareHandler(app *app.App) *MiddlewareHandler {
 	}
 }
 
+// IsAuthenticated is a function that checks whether the user has sufficient rights to access the endpoint
+/*
+ * c *fiber.Ctx - request context
+ * role string - the user role required for access
+ * tokenType string - the type of token that is required to access the endpoint
+ */
 func (h MiddlewareHandler) IsAuthenticated(c *fiber.Ctx, role string, tokenType string) error {
 	if len(c.GetReqHeaders()["Authorization"]) == 0 {
-		return
+		return errorz.AuthHeaderIsEmpty
 	}
 
 	authHeader := c.GetReqHeaders()["Authorization"][0]
 	if authHeader == "" {
-		c.Status(fiber.StatusUnauthorized)
-		return c.JSON(fiber.Map{
-			"status":  false,
-			"message": "auth header is empty",
-		})
+		return errorz.AuthHeaderIsEmpty
 	}
 
-	uuid, password, errParse := utils.ParseJwt(parts[1])
-	if errParse != nil {
+	id, errVerify := auth.VerifyToken(authHeader, viper.GetString("service.backend.jwt.secret"), tokenType)
+	if errVerify != nil {
 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"status": false,
-			"body":   errParse.Error(),
+			"success": false,
+			"message": errVerify.Error(),
 		})
 	}
 
-	user, errGetUser := h.userService.GetByUUID(c.Context(), uuid)
+	user, errGetUser := h.userService.GetByID(c.Context(), id)
 	if errGetUser != nil {
 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"status": false,
-			"body":   errGetUser.Error(),
+			"success": false,
+			"message": errGetUser.Error(),
 		})
 	}
 
-	if string(user.Password) != password {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"status": false,
-			"body":   errroz.TokenExpired.Error(),
+	if user.Role != role {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"success": false,
+			"message": errorz.Forbidden.Error(),
 		})
 	}
 	return c.Next()
 
-}
 }
--- internal/domain/common/errorz/errorz.go
+++ internal/domain/common/errorz/errorz.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	EmailAlreadyExists = errors.New("email already exists")
 	AuthHeaderIsEmpty  = errors.New("auth header is empty")
+	Forbidden          = errors.New("forbidden")
 )
